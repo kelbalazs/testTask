4911cf862c8b3e364b1c5f3e3d89f254f8a7afdb
Papp Ferenc <terencecleric@gmail.com>
2024-02-09 11:24:42 +0100

    Terence: debug

--- libs/php/neighboursAPI.php
+++ libs/php/neighboursAPI.php
@@ -1,47 +1,51 @@
 <?php
 
-// Remove for production
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
+try {
+    // Remove for production
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
 
-$executionStartTime = microtime(true);
+    $executionStartTime = microtime(true);
 
-// API endpoint URL
-$url = 'http://api.geonames.org/neighboursJSON?formatted=true&country=' . $_REQUEST['country'] . '&username=kelbal';
+    // API endpoint URL
+    $url = 'http://api.geonames.org/neighboursJSON?formatted=true&country=' . $_REQUEST['country'] . '&username=kelbal';
 
-// Initialize cURL session
-$ch = curl_init();
+    // Initialize cURL session
+    $ch = curl_init();
 
-// Set cURL options
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, $url);
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
 
-// Execute cURL session
-$result = curl_exec($ch);
+    // Execute cURL session
+    $result = curl_exec($ch);
 
-// Close cURL session
-curl_close($ch);
+    // Close cURL session
+    curl_close($ch);
 
-// Decode JSON response
-$decode = json_decode($result, true);
+    // Decode JSON response
+    $decode = json_decode($result, true);
 
-// debug
-if (!isset($decode['geonames'])) {
-    var_dump($decode);
+    // debug
+    if (!is_array($decode) || !isset($decode['geonames'])) {
+        var_dump($decode);
+    }
+
+    // Prepare output array
+    $output['status']['code'] = "200";
+    $output['status']['name'] = "ok";
+    $output['status']['description'] = "success";
+    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+    $output['data'] = $decode['geonames'];
+
+    // Set response header
+    header('Content-Type: application/json; charset=UTF-8');
+
+    // Output JSON response
+    echo json_encode($output);
+} catch (\Error $e) {
+    var_dump($e);
 }
-
-// Prepare output array
-$output['status']['code'] = "200";
-$output['status']['name'] = "ok";
-$output['status']['description'] = "success";
-$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode['geonames'];
-
-// Set response header
-header('Content-Type: application/json; charset=UTF-8');
-
-// Output JSON response
-echo json_encode($output);
 
 ?>
